3a17d0e96ac409f1e436f00c2deb6bbe783a7984
Whaltayr <josiwhatson@gmail.com>
2025-11-22 12:08:56 +0200

    Teste credential store

--- aula54.js
+++ aula54.js
@@ -1,10 +1,19 @@
-const caixa = document.getElementById("caixa")
+const caixa = document.getElementById("caixa");
 
-let mapa = new Map()
+let mapa = new Map();
 
-mapa.set("nome1" , "josy")
-mapa.set("nome2" , "vissuma")
-mapa.set("nome3" , "ferreira")
-mapa.set("nome4" , "eduardo")
+mapa.set("nome1", "josy");
+mapa.set("nome2", "vissuma");
+mapa.set("nome3", "ferreira");
+mapa.set("nome4", "eduardo");
 //map funciona con key and value, não use o mesmo nome para a key, se não o valor sera overwrited
 console.log(mapa);
+
+if (mapa.has("nome4")) {
+caixa.innerHTML="Valor encontrado"    
+}
+else{
+    caixa.innerHTML="Valor não encontrado"    
+}
+
+//caixa.innerHTML = mapa.get("nome1");
